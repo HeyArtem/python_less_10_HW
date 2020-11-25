fae84155c44aabf60fdd1da84d6926c08096207a
HeyArtem <artem_white@mail.ru>
2020-11-26 02:06:33 +0300

    ДЗ Pytest к игре Дурак  (game_fool).

--- test_unitest_fool.py
+++ test_unitest_fool.py
@@ -4,26 +4,50 @@ from game_fool import Deck
 
 class Test_game_fool(unittest.TestCase):
 
-    def test_fool_init(self):
-        '''Тест1. '''
+    def test_1_fool_init(self):
+        '''
+        Тест1
+        Создал карту с мастью (suit)=2
+        и достоинством (rank)=3
+        Здесь сравниваю suit
+        '''
         game_foll_unitest = Card(2,3)
-        # game_foll_unitest.self.suit = 2
         self.assertEqual(2, game_foll_unitest.suit)
 
-    def test_fool_rank(self):
+    def test_2_fool_rank(self):
+        '''
+        Тест 2
+        Создал карту с мастью (suit)=2
+        и достоинством (rank)=3
+        Здесь сравниваю rank
+        '''
         game_foll_unitest = Card(2, 3)
         self.assertEqual(3, game_foll_unitest.rank)
 
-    def test_fool_str(self):
+    def test_3_fool_str(self):
+        '''
+        Тест 3
+        Тестирую строчное представление карты
+        Создал карту Card(2(масть-Черви), 3(ранк-8))
+        '''
         game_foll_unitest = Card(2, 3)
-        # self.assertEqual('8 of Черви ♡', game_foll_unitest.__str__())
         self.assertEqual('8 of Черви ♡', str(game_foll_unitest))
 
-    def test_fool_init_Deck(self):
+    def test_4_fool_init_Deck(self):
+        '''
+        Тест 4
+        Тестирую количество карт в созданной колоде
+        Их должно быть 36
+        '''
         game_foll_unitest = Deck()
         self.assertEqual(36, (len(game_foll_unitest.cards)))
 
-    def test_fool_init_shuffle(self):
+    def test_5_fool_init_shuffle(self):
+        '''
+        Тест 5
+        Тестирую метод shuffle
+        тасую карты в колоде
+        '''
         game_foll_unitest = Deck()
         peremen = game_foll_unitest.cards
         peremen_2 = False
@@ -33,9 +57,9 @@ class Test_game_fool(unittest.TestCase):
         for i, j in zip(peremen, peremen_3):
             if i != j:
                 peremen_2 = True
-        self.assertEqual(True, peremen_2)
+        self.assertEqual(False, peremen_2)
 
-# возможно не работает шафл
+
 
 
 if __name__ == '__main__':
